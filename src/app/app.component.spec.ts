e9cfc21ab88193f3aa29783110020130075b5653
Nap Joseph Calub <njncalub@gmail.com>
2018-10-22 00:38:55 +0800

    Added prettier dependencies. Formatted current files.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,21 +4,21 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'werkflowy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     expect(app.title).toEqual('werkflowy');
   });
 
@@ -26,6 +26,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to werkflowy!');
+
+    expect(compiled.querySelector('h1').textContent).toContain('werkflowy');
   });
 });
